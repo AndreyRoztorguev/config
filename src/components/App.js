09a6c9054dce20efd919212382b60d152377c25f
Andrey Roztorguev <andrew.roztor99@gmail.com>
2019-11-08 22:43:08 +0200

    My Base configuration projects

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/App.css";
+import "../styles/myStyle.scss";
+
+class App extends React.Component {
+	render() {
+		return (
+			<div className="wrapper">
+				<h1 className="title">My React App!</h1>
+			</div>
+		);
+	}
+}
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const PrettierPlugin = require("prettier-webpack-plugin");
+
+
+module.exports = {
+    entry: ['./src/index.js'],
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'index_bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            }
+        ]
+    },
+    plugins: [
+        // Плагин и настройка html-webpack-plugin
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        /// Плагин и настройка Prettier
+        new PrettierPlugin({
+            printWidth: 80,
+            tabWidth: 2,
+            useTabs: true,
+            semi: true,
+            encoding: 'utf-8',
+            extensions: [".js", ".ts"]
+        })
+    ]
+
+};
